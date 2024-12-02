5007a3c3cc4adf15b599b1dcda5317546de6a05e
Mikhail Kortikov <kortikov.m.r@gmail.com>
2024-12-03 01:21:22 +0300

    TypeExtensions

---
+++ Utils/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace DingoUnityExtensions.Utils
+{
+    public static class TypeExtensions
+    {
+        public static string CSharpName(this Type type, bool isFullName = true)
+        {
+            var sb = new StringBuilder();
+            var name = isFullName ? type.FullName : type.Name;
+            if (!type.IsGenericType) return name;
+            name = name[..name.IndexOf('`')];
+            sb.Append(name);
+            var genericArguments = type.GetGenericArguments();
+            if (genericArguments.Length == 0 || genericArguments.Any(g => g.FullName == null))
+                return name;
+            sb.Append("<");
+            sb.Append(string.Join(", ", genericArguments.Select(t => t.CSharpName())));
+            sb.Append(">");
+            return sb.ToString();
+        }
+    }
+}
