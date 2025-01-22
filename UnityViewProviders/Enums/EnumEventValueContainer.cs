9e8824262790cd05cafa3206b14b5b0a94956b99
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-01-22 19:04:13 +0300

    EnumEventValueContainer

---
+++ UnityViewProviders/Enums/EnumEventValueContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AYellowpaper.SerializedCollections;
+using DingoUnityExtensions.UnityViewProviders.Core;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DingoUnityExtensions.UnityViewProviders.Enums
+{
+    public abstract class EnumEventValueContainer<TEnum> : ValueContainer<TEnum> where TEnum : Enum
+    {
+        [SerializeField] private SerializedDictionary<TEnum, UnityEvent> _eventsDict;
+        [SerializeField] private UnityEvent<TEnum> _events;
+
+        protected override void SetValueWithoutNotify(TEnum value)
+        {
+            if (_eventsDict.TryGetValue(value, out var e))
+                e.Invoke();
+            _events?.Invoke(value);
+        }
+    }
+}
