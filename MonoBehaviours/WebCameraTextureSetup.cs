1e620429aa0dd9889cf491b764b249955fda013d
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-02-03 17:37:46 +0300

    WebCameraTextureProvider

---
+++ MonoBehaviours/WebCameraTextureSetup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace MobileExerciseAnalyzerView.DebugElements
+{
+    public class WebCameraTextureSetup : MonoBehaviour
+    {
+        [SerializeField] private Material _material;
+        private WebCamTexture _webCamTexture;
+
+        private void Update()
+        {
+            // Check if at least one webcam is available
+            try
+            {
+                if (WebCamTexture.devices.Length > 0)
+                {
+                    _webCamTexture ??= new WebCamTexture();
+                    _webCamTexture.Play();
+                    if (!_webCamTexture.isPlaying)
+                        throw new Exception("Cannot play webcam texture");
+                    _material.mainTexture = _webCamTexture;
+                }
+                else
+                {
+                    Debug.LogWarning("No webcam detected on this device.");
+                    _webCamTexture = null;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                enabled = false;
+            }
+        }
+    }
+}
