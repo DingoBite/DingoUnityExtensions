88f267767b20f7338b801b7e74955dfee80b63e7
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-02-13 00:07:40 +0300

    Cache improve

--- Serialization/SerializationUtils.cs
+++ Serialization/SerializationUtils.cs
@@ -1,6 +1,7 @@
 ﻿#if NEWTONSOFT_EXISTS
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,13 +17,110 @@ namespace DingoUnityExtensions.Serialization
         ForceUpdateCachedValue,
     }
 
+    public enum CacheType
+    {
+        Queue,
+        Stack,
+        Random
+    }
+
     public static class SerializationUtils
     {
         private static class StaticCache<T>
         {
-            public static readonly ConcurrentDictionary<string, T> CachedValues = new();
+            private const int MAX_ITERATIONS = 50;
+            
+            private static readonly ConcurrentDictionary<string, T> CachedValues = new();
+            private static readonly ConcurrentQueue<string> Queue = new();
+            private static readonly ConcurrentStack<string> Stack = new();
+            private static readonly ConcurrentBag<string> Bag = new();
+            
+            private static int _capacity = -1;
+            private static CacheType _cacheType = CacheType.Random;
+            private static Action<T> _dispose;
+
+            public static void Setup(int capacity, CacheType cacheType, Action<T> dispose)
+            {
+                _dispose = dispose;
+                _cacheType = cacheType;
+                _capacity = capacity;
+            }
+
+            public static bool TryGetCache(string key, out T value)
+            {
+                return CachedValues.TryGetValue(key, out value);
+            }
+
+            public static void AddCacheValue(string key, T value)
+            {
+                if (_capacity > 0)
+                {
+                    var iterations = MAX_ITERATIONS;
+                    while (iterations >= 0 && CachedValues.Count >= _capacity)
+                    {
+                        iterations--;
+                        switch (_cacheType)
+                        {
+                            case CacheType.Queue:
+                                QueueBasedClear();
+                                break;
+                            case CacheType.Stack:
+                                StackBasedClear();
+                                break;
+                            case CacheType.Random:
+                                RandomBasedClear();
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                }
+
+                CachedValues.AddOrUpdate(key, k => value, (k, v) =>
+                {
+                    _dispose?.Invoke(v);
+                    return value;
+                });
+                switch (_cacheType)
+                {
+                    case CacheType.Queue:
+                        Queue.Enqueue(key);
+                        break;
+                    case CacheType.Stack:
+                        Stack.Push(key);
+                        break;
+                    case CacheType.Random:
+                        Bag.Add(key);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            private static void RandomBasedClear()
+            {
+                if (Bag.TryTake(out var removeKey) && CachedValues.TryRemove(removeKey, out var valueToDispose))
+                    _dispose?.Invoke(valueToDispose);
+            }
+
+            private static void QueueBasedClear()
+            {
+                if (Queue.TryDequeue(out var removeKey) && CachedValues.TryRemove(removeKey, out var valueToDispose))
+                    _dispose?.Invoke(valueToDispose);
+            }
+
+            private static void StackBasedClear()
+            {
+                if (Stack.TryPop(out var removeKey) && CachedValues.TryRemove(removeKey, out var valueToDispose))
+                    _dispose?.Invoke(valueToDispose);
+            }
         }
 
+        public static void SetupCache<T>(int capacity, CacheType cacheType, Action<T> dispose = null)
+        {
+            StaticCache<T>.Setup(capacity, cacheType, dispose);
+        }
+        
         public static async Task SaveSerializeAsync(string path, object obj, bool catchException = true, JsonSerializerSettings settings = null, CancellationTokenSource cancellationTokenSource = null)
         {
             string json;
@@ -63,7 +161,7 @@ namespace DingoUnityExtensions.Serialization
         
         public static async Task<T> DeserializeFromFileAsync<T>(string path, CacheOption cacheOption = CacheOption.None, bool catchException = true, T defaultValue = default, JsonSerializerSettings settings = null, CancellationTokenSource cancellationTokenSource = null)
         {
-            if (cacheOption == CacheOption.TryGetCachedValue && StaticCache<T>.CachedValues.TryGetValue(path, out var cachedValue))
+            if (cacheOption == CacheOption.TryGetCachedValue && StaticCache<T>.TryGetCache(path, out var cachedValue))
                 return cachedValue;
             
             if (!File.Exists(path))
@@ -106,15 +204,15 @@ namespace DingoUnityExtensions.Serialization
                 }
             }
 
-            if (cacheOption == CacheOption.ForceUpdateCachedValue || cacheOption == CacheOption.TryGetCachedValue && !StaticCache<T>.CachedValues.ContainsKey(path))
-                StaticCache<T>.CachedValues.AddOrUpdate(path, value, (k, v) => value);
+            if (cacheOption == CacheOption.ForceUpdateCachedValue || cacheOption == CacheOption.TryGetCachedValue && !StaticCache<T>.TryGetCache(path, out _))
+                StaticCache<T>.AddCacheValue(path, value);
 
             return value;
         }
         
         public static T DeserializeOrDefault<T>(string key, string json, CacheOption cacheOption = CacheOption.None, bool catchException = true, T defaultValue = default, JsonSerializerSettings settings = null)
         {
-            if (cacheOption == CacheOption.TryGetCachedValue && StaticCache<T>.CachedValues.TryGetValue(key, out var cachedValue))
+            if (cacheOption == CacheOption.TryGetCachedValue && StaticCache<T>.TryGetCache(key, out var cachedValue))
                 return cachedValue;
             
             T value;
@@ -135,8 +233,8 @@ namespace DingoUnityExtensions.Serialization
                 value = settings == null ? JsonConvert.DeserializeObject<T>(json) : JsonConvert.DeserializeObject<T>(json, settings);
             }
 
-            if (cacheOption == CacheOption.ForceUpdateCachedValue || cacheOption == CacheOption.TryGetCachedValue && !StaticCache<T>.CachedValues.ContainsKey(key))
-                StaticCache<T>.CachedValues.AddOrUpdate(key, value, (k, v) => value);
+            if (cacheOption == CacheOption.ForceUpdateCachedValue || cacheOption == CacheOption.TryGetCachedValue && !StaticCache<T>.TryGetCache(key, out _))
+                StaticCache<T>.AddCacheValue(key, value);
 
             return value;
         }
