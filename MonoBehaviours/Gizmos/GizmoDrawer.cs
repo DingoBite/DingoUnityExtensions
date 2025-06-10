08738aa4a5d8c7812aeef73a7100941720f920db
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-06-11 01:05:24 +0300

    GizmoDrawer

--- MonoBehaviours/Gizmos/GizmoDrawer.cs
+++ MonoBehaviours/Gizmos/GizmoDrawer.cs
@@ -7,6 +7,7 @@ namespace DingoUnityExtensions.MonoBehaviours.Gizmos
 {
     public class GizmosDrawer : SingletonBehaviour<GizmosDrawer>
     {
+        private readonly Dictionary<object, Action> _drawPersistentActions = new();
         private readonly List<Action> _drawActions = new();
         
         public static void AddCall(Action drawAction)
@@ -16,9 +17,34 @@ namespace DingoUnityExtensions.MonoBehaviours.Gizmos
 #endif
         }
 
+        public static void AddPersistentCall(object key, Action drawAction)
+        {
+#if UNITY_EDITOR
+            Instance._drawPersistentActions[key] = drawAction;
+#endif
+        }
+
+        public static void RemovePersistentCall(object key)
+        {
+#if UNITY_EDITOR
+            Instance._drawPersistentActions.Remove(key);
+#endif
+        }
+        
 #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
+            foreach (var drawAction in _drawPersistentActions.Values)
+            {
+                try
+                {
+                    drawAction();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
             foreach (var drawAction in _drawActions)
             {
                 try
