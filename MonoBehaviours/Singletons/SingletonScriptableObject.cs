49386d15ee7850fb10f24b6a90181d1fbad32e35
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-07-03 18:59:18 +0300

    Singleton ScriptableObject

--- MonoBehaviours/Singletons/SingletonScriptableObject.cs
+++ MonoBehaviours/Singletons/SingletonScriptableObject.cs
@@ -6,60 +6,34 @@ namespace DingoUnityExtensions.MonoBehaviours.Singletons
     {
         private static T _instance;
         private static readonly object _lock = new();
-        private static bool _applicationIsQuitting;
-
-        static SingletonScriptableObject()
-        {
-            Application.quitting += OnApplicationQuit;
-        }
-
+        
         public static T Instance
         {
             get
             {
-                if (_applicationIsQuitting)
-                {
-                    Debug.LogWarning($"[Singleton] Instance of '{typeof(T)}' already destroyed. Returning null.");
-                    return null;
-                }
-
                 lock (_lock)
                 {
-                    if (_instance == null)
-                    {
-                        var instances = Resources.FindObjectsOfTypeAll<T>();
+                    if (_instance != null)
+                        return _instance;
+                    var instances = Resources.FindObjectsOfTypeAll<T>();
 
-                        if (instances.Length > 1)
-                        {
-                            Debug.LogError($"[Singleton] Multiple instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
-                            _instance = instances[0];
-                        }
-                        else if (instances.Length == 1)
-                        {
-                            _instance = instances[0];
-                        }
-                        else if (Application.isPlaying)
-                        {
-                            Debug.LogError($"[Singleton] No instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
-                        }
+                    if (instances.Length > 1)
+                    {
+                        Debug.LogError($"[Singleton] Multiple instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
+                        _instance = instances[0];
+                    }
+                    else if (instances.Length == 1)
+                    {
+                        _instance = instances[0];
+                    }
+                    else if (Application.isPlaying)
+                    {
+                        Debug.LogError($"[Singleton] No instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
                     }
 
                     return _instance;
                 }
             }
-        }
-
-        protected virtual void OnDisable()
-        {
-            if (_instance == this)
-            {
-                _applicationIsQuitting = true;
-            }
-        }
-
-        private static void OnApplicationQuit()
-        {
-            _applicationIsQuitting = true;
         }
     }
 
@@ -67,60 +41,34 @@ namespace DingoUnityExtensions.MonoBehaviours.Singletons
     {
         private static T _instance;
         private static readonly object _lock = new();
-        private static bool _applicationIsQuitting;
-
-        static ProtectedSingletonScriptableObject()
-        {
-            Application.quitting += OnApplicationQuit;
-        }
 
         protected static T Instance
         {
             get
             {
-                if (_applicationIsQuitting)
-                {
-                    Debug.LogWarning($"[Singleton] Instance of '{typeof(T)}' already destroyed. Returning null.");
-                    return null;
-                }
-
                 lock (_lock)
                 {
-                    if (_instance == null)
-                    {
-                        var instances = Resources.FindObjectsOfTypeAll<T>();
+                    if (_instance != null)
+                        return _instance;
+                    var instances = Resources.FindObjectsOfTypeAll<T>();
 
-                        if (instances.Length > 1)
-                        {
-                            Debug.LogError($"[Singleton] Multiple instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
-                            _instance = instances[0];
-                        }
-                        else if (instances.Length == 1)
-                        {
-                            _instance = instances[0];
-                        }
-                        else if (Application.isPlaying)
-                        {
-                            Debug.LogError($"[Singleton] No instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
-                        }
+                    if (instances.Length > 1)
+                    {
+                        Debug.LogError($"[Singleton] Multiple instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
+                        _instance = instances[0];
+                    }
+                    else if (instances.Length == 1)
+                    {
+                        _instance = instances[0];
+                    }
+                    else if (Application.isPlaying)
+                    {
+                        Debug.LogError($"[Singleton] No instances of singleton '{typeof(T)}' found. This can lead to incorrect behavior.");
                     }
 
                     return _instance;
                 }
             }
-        }
-
-        protected virtual void OnDisable()
-        {
-            if (_instance == this)
-            {
-                _applicationIsQuitting = true;
-            }
-        }
-
-        private static void OnApplicationQuit()
-        {
-            _applicationIsQuitting = true;
         }
     }
 }
