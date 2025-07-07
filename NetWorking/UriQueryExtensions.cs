b5e8421d96eebb5f414265aba9ecec474ff8e584
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-07-07 16:03:02 +0300

    NetWorking

---
+++ NetWorking/UriQueryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace DingoUnityExtensions.NetWorking
+{
+    public static class UriQueryExtensions
+    {
+        public static NameValueCollection AppendParameters(this NameValueCollection nameValueCollection, IEnumerable<KeyValuePair<string, string>> parameters)
+        {
+            foreach (var (key, value) in parameters)
+            {
+                nameValueCollection[key] = value;
+            }
+            return nameValueCollection;
+        }
+
+        public static NameValueCollection AppendParameters(this NameValueCollection nameValueCollection, IEnumerable<(string, string)> parameters)
+        {
+            foreach (var (key, value) in parameters)
+            {
+                nameValueCollection[key] = value;
+            }
+            return nameValueCollection;
+        }
+    }
+}
