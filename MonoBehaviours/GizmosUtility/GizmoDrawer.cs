aab99f00ddfb686901c6a1a3d8a99a3e1aabe3be
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-07-28 00:08:00 +0300

    Gizmos

---
+++ MonoBehaviours/GizmosUtility/GizmoDrawer.cs
@@ -0,0 +1,133 @@
+﻿#if UNITY_EDITOR
+#endif
+using System;
+using System.Collections.Generic;
+using DingoUnityExtensions.MonoBehaviours.Singletons;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace DingoUnityExtensions.MonoBehaviours.GizmosUtility
+{
+    [ExecuteAlways]
+    public class GizmosDrawer : SingletonBehaviour<GizmosDrawer>
+    {
+        private readonly Dictionary<object, Action> _drawPersistentActions = new();
+        private readonly List<Action> _drawActions = new();
+        
+        private readonly Dictionary<Object, Action> _drawPersistentSelectedActions = new();
+        private readonly Dictionary<Object, Action> _drawSelectedActions = new();
+
+        private void Awake()
+        {
+            _drawPersistentActions.Clear();
+            _drawActions.Clear();
+            
+            _drawPersistentSelectedActions.Clear();
+            _drawSelectedActions.Clear();
+        }
+        
+        public static void AddCall(Action drawAction)
+        {
+#if UNITY_EDITOR
+            Instance._drawActions.Add(drawAction);
+#endif
+        }
+
+        public static void AddPersistentCall(object key, Action drawAction)
+        {
+#if UNITY_EDITOR
+            Instance._drawPersistentActions[key] = drawAction;
+#endif
+        }
+
+        public static void RemovePersistentCall(object key)
+        {
+#if UNITY_EDITOR
+            Instance._drawPersistentActions.Remove(key);
+#endif
+        }
+        
+        public static void AddCallWhenSelected(Object key, Action drawAction)
+        {
+#if UNITY_EDITOR
+            Instance._drawSelectedActions[key] = drawAction;
+#endif
+        }
+
+        public static void AddPersistentCallWhenSelected(Object key, Action drawAction)
+        {
+#if UNITY_EDITOR
+            Instance._drawPersistentSelectedActions[key] = drawAction;
+#endif
+        }
+
+        public static void RemovePersistentCallWhenSelected(Object key)
+        {
+#if UNITY_EDITOR
+            Instance._drawPersistentSelectedActions.Remove(key);
+#endif
+        }
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            foreach (var drawAction in _drawPersistentActions.Values)
+            {
+                try
+                {
+                    drawAction();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+            
+            foreach (var drawAction in _drawActions)
+            {
+                try
+                {
+                    drawAction();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+            _drawActions.Clear();
+            
+            foreach (var (obj, drawAction) in _drawPersistentSelectedActions)
+            {
+                if (Selection.activeObject != obj)
+                    continue;
+                
+                try
+                {
+                    drawAction();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+            
+            foreach (var (obj, drawAction) in _drawSelectedActions)
+            {
+                if (Selection.activeObject != obj)
+                    continue;
+                
+                try
+                {
+                    drawAction();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+            _drawSelectedActions.Clear();
+        }
+#endif
+    }
+}
---
+++ MonoBehaviours/GizmosUtility/GizmosUtils.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DingoUnityExtensions.MathAndGeometry;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace DingoUnityExtensions.MonoBehaviours.GizmosUtility
+{
+    public static class GizmosUtils
+    {
+        // Make something like this:
+        //      GizmosUtils.EachSegmentCallback += OnEachSegmentCallback;
+        //      GizmosUtils.DrawGrid2D(-100 * Vector2.one, 100 * Vector2.one, Vector2.one * Config.GridStep, 0, _gridColor);
+        //      GizmosUtils.EachSegmentCallback -= OnEachSegmentCallback;
+        public static event Func<object, Color, Color> EachSegmentCallback;
+
+        public static Color WrapEachSegmentCallback(object obj, Color color, Func<Tuple<Vector3, Vector3>, Color> func) => WrapEachSegmentCallback<Tuple<Vector3, Vector3>>(obj, color, func);
+        
+        public static Color WrapEachSegmentCallback<T>(object obj, Color color, Func<T, Color> func)
+        {
+            try
+            {
+                var value = (T)obj;
+                return func(value);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+            return color;
+        }
+        
+        public static void DrawEllipsoid(Vector3 pos, Quaternion rotation, Vector3 size, int segments, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var f = Vector3.forward;
+            var u = Vector3.up;
+            var r = Vector3.right;
+            if (rotation != Quaternion.identity)
+            {
+                f = rotation * f;
+                u = rotation * u;
+                r = rotation * r;
+            }
+            
+            DrawEllipse(pos, f, u, size.x, size.y, segments, color);
+            DrawEllipse(pos, r, f, size.y, size.z, segments, color);
+            DrawEllipse(pos, u, r, size.z, size.x, segments, color);
+        }
+        
+        public static void DrawEllipse(Vector3 pos, Vector3 forward, Vector3 up, float radiusX, float radiusY, int segments, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var angle = 0f;
+            var rot = Quaternion.LookRotation(forward, up);
+            var lastPoint = Vector3.zero;
+            var thisPoint = Vector3.zero;
+
+            for (var i = 0; i < segments + 1; i++)
+            {
+                thisPoint.x = Mathf.Sin(Mathf.Deg2Rad * angle) * radiusX;
+                thisPoint.y = Mathf.Cos(Mathf.Deg2Rad * angle) * radiusY;
+
+                if (i > 0)
+                {
+                    var start = rot * lastPoint + pos;
+                    var end = rot * thisPoint + pos;
+                    if (EachSegmentCallback != null)
+                        color = EachSegmentCallback.Invoke((start, end), color);
+                    DrawSegment(start, end, color);
+                }
+
+                lastPoint = thisPoint;
+                angle += 360f / segments;
+            }
+        }
+        
+        public static void DrawSegment(Vector3 from, Vector3 to, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            Gizmos.DrawLine(from, to);
+            Gizmos.color = prevColor;
+        }
+
+        public static void DrawSphere(Vector3 center, float radius, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            Gizmos.DrawSphere(center, radius);
+            Gizmos.color = prevColor;
+        }
+
+        public static void DrawCube(Vector3 center, Vector3 size, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            Gizmos.DrawCube(center, size);
+            Gizmos.color = prevColor;
+        }
+
+        public static void DrawMesh(Mesh mesh, Vector3 center, Vector3 forwardRotation, Vector3 scale, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            var rotation = Quaternion.LookRotation(forwardRotation.normalized);
+            Gizmos.DrawMesh(mesh, center, rotation, scale);
+            Gizmos.color = prevColor;
+        }
+
+        public static void DrawSegmentsLine(Vector3[] points, Color color, bool looped = false)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            if (points.Length == 0)
+                return;
+
+            for (var i = 1; i < points.Length; i++)
+            {
+                var p0 = points[i - 1];
+                var p1 = points[i];
+                if (EachSegmentCallback != null)
+                    color = EachSegmentCallback.Invoke((p0, p1), color);
+                DrawSegment(p0, p1, color);
+            }
+
+            if (looped)
+            {
+                var pStart = points[0];
+                var pEnd = points[^1];
+                if (EachSegmentCallback != null)
+                    color = EachSegmentCallback.Invoke((pStart, pEnd), color);
+                DrawSegment(pStart, pEnd, color);
+            }
+        }
+
+        public static void DrawSegmentsLine(IEnumerable<MonoBehaviour> points, Color color, bool looped = false)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var pointsArray = points.Select(p => p.transform.position).ToArray();
+            DrawSegmentsLine(pointsArray, color, looped);
+        }
+
+        public static void DrawLine(Vector3 center, Vector3 forward, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            Gizmos.DrawRay(center, forward * 1000);
+            Gizmos.DrawRay(center, -forward * 1000);
+            Gizmos.color = prevColor;
+        }
+
+        public static void DrawFrustum(Vector3 center, Quaternion rotation, float fov, float maxRange, float minRange, float aspect, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            var prevMatrix = Gizmos.matrix;
+            Gizmos.color = color;
+            Gizmos.matrix = Matrix4x4.TRS(center, rotation, Vector3.one);
+
+            Gizmos.DrawFrustum(Vector3.zero, fov, maxRange, minRange, aspect);
+
+            Gizmos.matrix = prevMatrix;
+            Gizmos.color = prevColor;
+        }
+
+        public static void DrawPlane(Vector3 position, Vector2 dimensions, Quaternion rotation, Color color, bool isDrawCross)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            var points = GeometricUtils.GetBorderPoints(position, dimensions, rotation);
+
+            if (isDrawCross)
+            {
+                var right = points[3] - points[0];
+                var up = points[1] - points[0];
+                DrawSegment(points[0] + 0.5f * up, points[0] + right + 0.5f * up, color * 0.7f);
+                DrawSegment(points[0] + 0.5f * right, points[0] + up + 0.5f * right, color * 0.7f);
+            }
+
+            DrawSegmentsLine(points, color, true);
+
+            Gizmos.color = prevColor;
+        }
+        
+        public static void DrawGrid2D(Vector2 minXZ, Vector2 maxXZ, Vector2 cellSize, float gridY, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            float x0 = minXZ.x, x1 = maxXZ.x;
+            float z0 = minXZ.y, z1 = maxXZ.y;
+            for (var x = x0; x <= x1; x += cellSize.x)
+            {
+                var p0 = new Vector3(x, gridY, z0);
+                var p1 = new Vector3(x, gridY, z1);
+                if (EachSegmentCallback != null)
+                    color = EachSegmentCallback.Invoke((p0, p1), color);
+                DrawSegment(p0, p1, color);
+            }
+            for (var z = z0; z <= z1; z += cellSize.y)
+            {
+                var p0 = new Vector3(x0, gridY, z);
+                var p1 = new Vector3(x1, gridY, z);
+                if (EachSegmentCallback != null)
+                    color = EachSegmentCallback.Invoke((p0, p1), color);
+                DrawSegment(p0, p1, color);
+            }
+        }
+        
+        public static void DrawGrid3D(Vector3 min, Vector3 max, Vector3 cellSize, Color color)
+        {
+            if (color.a < Vector2.kEpsilon || cellSize.x <= 0 || cellSize.y <= 0 || cellSize.z <= 0)
+                return;
+            min = new Vector3(min.x * cellSize.x, min.y * cellSize.y, min.z *= cellSize.z);
+            max = new Vector3(max.x * cellSize.x, max.y * cellSize.y, max.z *= cellSize.z);
+            for (var z = min.z; z <= max.z; z += cellSize.z)
+            {
+                for (var y = min.y; y <= max.y; y += cellSize.y)
+                {
+                    var p0 = new Vector3(min.x, y, z);
+                    var p1 = new Vector3(max.x, y, z);
+                    if (EachSegmentCallback != null)
+                        color = EachSegmentCallback.Invoke((p0, p1), color);
+                    DrawSegment(p0, p1, color);
+                }
+            }
+
+            for (var x = min.x; x <= max.x; x += cellSize.x)
+            {
+                for (var y = min.y; y <= max.y; y += cellSize.y)
+                {
+                    var p0 = new Vector3(x, y, min.z);
+                    var p1 = new Vector3(x, y, max.z);
+                    if (EachSegmentCallback != null)
+                        color = EachSegmentCallback.Invoke((p0, p1), color);
+                    DrawSegment(p0, p1, color);
+                }
+            }
+
+            for (var x = min.x; x <= max.x; x += cellSize.x)
+            {
+                for (var z = min.z; z <= max.z; z += cellSize.z)
+                {
+                    var p0 = new Vector3(x, min.y, z);
+                    var p1 = new Vector3(x, max.y, z);
+                    if (EachSegmentCallback != null)
+                        color = EachSegmentCallback.Invoke((p0, p1), color);
+                    DrawSegment(p0, p1, color);
+                }
+            }
+        }
+
+        public static bool TryGetSelectedObjectPosition(out Vector3 position)
+        {
+            position = Selection.activeTransform != null ? Selection.activeTransform.position : Vector3.zero;
+            return Selection.activeTransform != null;
+        }
+        
+        public static bool TryViewportToWorldPointOnPlane(Vector2 viewportPoint, Plane plane, out Vector3 position)
+        {
+            var sceneView = SceneView.lastActiveSceneView;
+            if (sceneView == null || sceneView.camera == null)
+            {
+                position = Vector3.zero;
+                return false;
+            }
+
+            var cam = sceneView.camera;
+            var ray = cam.ViewportPointToRay(new Vector3(viewportPoint.x, viewportPoint.y, 0f));
+            if (plane.Raycast(ray, out var enter))
+            {
+                position = ray.GetPoint(enter);
+                return true;
+            }
+
+            position = Vector3.zero;
+            return false;
+        }
+
+        public static void DrawText(Vector3 position, string text, Color color)
+        {
+            if (color.a < Vector2.kEpsilon)
+                return;
+            var prev = Handles.color;
+            GUI.color = color;
+            Handles.Label(position, text);
+            GUI.color = prev;
+        }
+    }
+}
