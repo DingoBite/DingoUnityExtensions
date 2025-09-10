51bc865f13397bb89a938c8b644b01d2b84c6332
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-09-10 22:25:00 +0300

    AsyncValueContainer

---
+++ UnityViewProviders/AsyncValueContainer/AsyncValueStateContainer.cs
@@ -0,0 +1,8 @@
+using DingoUnityExtensions.UnityViewProviders.Enums;
+
+namespace DingoUnityExtensions.UnityViewProviders.AsyncValueContainer
+{
+    public class AsyncValueStateContainer : EnumToggleValueContainer<AsyncValueState>
+    {
+    }
+}
