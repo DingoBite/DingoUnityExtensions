c4a66cf3161337bff30928d897effca2c5847c8b
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-09-12 20:15:49 +0300

    Serialization and Files working improvements

---
+++ Utils/SafeDirectory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace DingoUnityExtensions.Utils
+{
+    public static class SafeDirectory
+    {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+        private static readonly StringComparison PathCmp = StringComparison.OrdinalIgnoreCase;
+#else
+        private static readonly StringComparison PathCmp = StringComparison.Ordinal;
+#endif
+
+        public static void Delete(string path, string basePath, bool recursive = false)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException("Path is empty.", nameof(path));
+
+            basePath = basePath.NormalizePath();
+            var full = path.NormalizePath();
+            
+            if (!IsUnderBase(full, basePath))
+                throw new UnauthorizedAccessException($"Refusing to delete outside of persistentDataPath.\nBase: {basePath}\nRequested: {full}");
+
+            if (!Directory.Exists(full))
+                return;
+
+            Directory.Delete(full, recursive);
+        }
+
+        private static bool IsUnderBase(string fullPath, string basePath)
+        {
+            return fullPath.StartsWith(basePath, PathCmp);
+        }
+    }
+}
--- Utils/TypeExtensions.cs
+++ Utils/TypeExtensions.cs
@@ -6,7 +6,7 @@ namespace DingoUnityExtensions.Utils
 {
     public static class TypeExtensions
     {
-        public static string CSharpName(this Type type, bool isFullName = true)
+        public static string CSharpName(this Type type, bool isFullName = true, bool genericChars = true)
         {
             var sb = new StringBuilder();
             var name = isFullName ? type.FullName : type.Name;
@@ -14,7 +14,7 @@ namespace DingoUnityExtensions.Utils
             name = name[..name.IndexOf('`')];
             sb.Append(name);
             var genericArguments = type.GetGenericArguments();
-            if (genericArguments.Length == 0 || genericArguments.Any(g => g.FullName == null))
+            if (!genericChars || genericArguments.Length == 0 || genericArguments.Any(g => g.FullName == null))
                 return name;
             sb.Append("<");
             sb.Append(string.Join(", ", genericArguments.Select(t => t.CSharpName())));
