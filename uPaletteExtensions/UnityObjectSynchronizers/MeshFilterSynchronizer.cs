67b6f8297c91f5fdffabdd14ceaddbfd267184b8
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-12-11 18:50:46 +0300

    Theme Rules uPalette

---
+++ uPaletteExtensions/UnityObjectSynchronizers/MeshFilterSynchronizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using uPalette.Runtime.Core.Synchronizer.UnityObject;
+
+namespace DingoUnityExtensions.uPaletteExtensions.UnityObjectSynchronizers
+{
+    [RequireComponent(typeof(MeshFilter))]
+    [UnityObjectSynchronizer(typeof(MeshFilter), "Mesh")]
+    public class MeshFilterSynchronizer : UnityObjectSynchronizer<MeshFilter>
+    {
+        [SerializeField] private bool _sharedMeshInPlayMode;
+        
+        public override Object GetValue()
+        {
+            if (Application.isPlaying && !_sharedMeshInPlayMode)
+                return Component.mesh;
+            else 
+                return Component.sharedMesh;
+        }
+
+        public override void SetValue(Object value)
+        {
+            if (Application.isPlaying && !_sharedMeshInPlayMode)
+                Component.mesh = value as Mesh;
+            else 
+                Component.sharedMesh = value as Mesh;
+        }
+    }
+}
