8fa1e5103820777685d4101b28b055eadfb2f213
Mikhail Kortikov <kortikov.m.r@gmail.com>
2026-01-29 03:02:27 +0300

    JsonOptions improve

--- Serialization/JsonOptions.cs
+++ Serialization/JsonOptions.cs
@@ -1,6 +1,8 @@
 #if NEWTONSOFT_EXISTS
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.UnityConverters.Math;
 
@@ -34,10 +36,60 @@ namespace DingoUnityExtensions.Serialization
 
         private static void AddUnityConverters(JsonSerializerSettings settings)
         {
-            settings.Converters.Add(new Vector2Converter());
-            settings.Converters.Add(new Vector3Converter());
-            settings.Converters.Add(new QuaternionConverter());
-            settings.Converters.Add(new Matrix4x4Converter());
+            AddAllConvertersFromNamespace(settings, "Newtonsoft.Json.UnityConverters.Mathematics");
+        }
+        
+        public static void AddAllConvertersFromNamespace(JsonSerializerSettings settings, string targetNamespace)
+        {
+            var existing = new HashSet<Type>();
+            for (var i = 0; i < settings.Converters.Count; i++)
+                existing.Add(settings.Converters[i].GetType());
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            for (var a = 0; a < assemblies.Length; a++)
+            {
+                var asm = assemblies[a];
+                var asmName = asm.GetName().Name ?? string.Empty;
+
+                if (asmName.IndexOf("UnityConverters", StringComparison.OrdinalIgnoreCase) < 0 && asmName.IndexOf("Newtonsoft", StringComparison.OrdinalIgnoreCase) < 0)
+                    continue;
+
+                Type[] types;
+                try
+                {
+                    types = asm.GetTypes();
+                }
+                catch (ReflectionTypeLoadException e)
+                {
+                    types = e.Types;
+                }
+
+                if (types == null)
+                    continue;
+
+                for (var t = 0; t < types.Length; t++)
+                {
+                    var type = types[t];
+                    if (type == null)
+                        continue;
+
+                    if (type.IsAbstract)
+                        continue;
+                    if (type.IsGenericTypeDefinition)
+                        continue;
+                    if (type.Namespace != targetNamespace)
+                        continue;
+                    if (!typeof(JsonConverter).IsAssignableFrom(type))
+                        continue;
+                    if (type.GetConstructor(Type.EmptyTypes) == null)
+                        continue;
+                    if (existing.Contains(type))
+                        continue;
+
+                    settings.Converters.Add((JsonConverter)Activator.CreateInstance(type));
+                    existing.Add(type);
+                }
+            }
         }
     }
 }
