e57b854809e02125a5cecf022276d2b9c899f914
Mikhail Kortikov <kortikov.m.r@gmail.com>
2026-07-26 15:27:41 +0300

    OnLoad fix

--- Editor/OnLoad.cs
+++ Editor/OnLoad.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.Build;
 
@@ -9,99 +10,105 @@ namespace DingoUnityExtensions.Editor
     [InitializeOnLoad]
     public class OnLoad : UnityEditor.Editor
     {
+        /// Walking every type of every loaded assembly costs seconds, and [InitializeOnLoad] runs it
+        /// on every domain reload. The answer can only change when the set of loaded assemblies
+        /// changes, so the scan is skipped while that set stays the same within an editor session.
+        private const string AssembliesKey = "DingoUnityExtensions.OnLoad.ScannedAssemblies";
+
+        private static readonly (string Namespace, string Define)[] NamespaceDefines =
+        {
+            ("Cysharp.Threading.Tasks", "UNITASK_EXISTS"),
+            ("Newtonsoft.Json.Serialization", "NEWTONSOFT_EXISTS"),
+            ("NaughtyAttributes", "NAUGHTYATTRIBUTES_EXISTS"),
+            ("NaughtyAttributes.ColorKeyProperties", "NAUGHTYATTRIBUTES_CK_EXISTS"),
+            ("Bind", "BIND_EXISTS"),
+            ("UnityEngine.ResourceManagement.AsyncOperations", "ADDRESSABLES_EXISTS"),
+            ("VInspector", "VINSPECTOR_EXISTS"),
+            ("UnityEngine.UI.ProceduralImage", "PROCEDURAL_IMAGE_EXISTS"),
+            ("DTT.UI.ProceduralUI", "PROCEDURAL_UI_EXISTS"),
+            ("MoreMountains.Feedbacks", "MMFEEL_EXISTS"),
+        };
+
         static OnLoad()
         {
-            ManualProcessDefines();
+            ProcessDefines(false);
         }
 
         [MenuItem("Edit/Manual Process Defines")]
         private static void ManualProcessDefines()
         {
+            ProcessDefines(true);
+        }
+
+        private static void ProcessDefines(bool force)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var loadedAssemblies = GetAssemblySignature(assemblies);
+            if (!force && SessionState.GetString(AssembliesKey, string.Empty) == loadedAssemblies)
+                return;
+
 #if UNITY_6000_0_OR_NEWER
             var buildTargetGroup = NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
             var definesString = PlayerSettings.GetScriptingDefineSymbols(buildTargetGroup);
-            var allDefines = definesString.Split(';').ToList();
-            allDefines.AddRange(GetSymbols().Except(allDefines));
-            PlayerSettings.SetScriptingDefineSymbols(
-                buildTargetGroup,
-                string.Join(";", allDefines.ToArray()));
-#else 
+#else
             var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            var allDefines = definesString.Split(';').ToList();
-            allDefines.AddRange(GetSymbols().Except(allDefines));
+#endif
+            var allDefines = definesString
+                .Split(';')
+                .Where(define => !string.IsNullOrWhiteSpace(define))
+                .ToList();
+            allDefines.AddRange(GetSymbols(assemblies).Except(allDefines));
+
+            SessionState.SetString(AssembliesKey, loadedAssemblies);
+
+            // Writing the same value back still dirties the settings and can cost a recompile.
+            var updatedDefines = string.Join(";", allDefines);
+            if (updatedDefines == definesString)
+                return;
+
+#if UNITY_6000_0_OR_NEWER
+            PlayerSettings.SetScriptingDefineSymbols(buildTargetGroup, updatedDefines);
+#else
             PlayerSettings.SetScriptingDefineSymbolsForGroup(
                 EditorUserBuildSettings.selectedBuildTargetGroup,
-                string.Join(";", allDefines.ToArray()));
+                updatedDefines);
 #endif
-            
         }
 
-        private static IEnumerable<string> GetSymbols()
+        private static string GetAssemblySignature(Assembly[] assemblies)
         {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var types = (from assembly in assemblies
-                from type in assembly.GetTypes()
-                select type).ToArray();
-            
-            var unitaskNamespaceFound = (from type in types
-                where type.Namespace == "Cysharp.Threading.Tasks"
-                select type).Any();
-            if (unitaskNamespaceFound)
-                yield return "UNITASK_EXISTS";
-            
-            var newtonsoftNamespaceFound = (from type in types
-                where type.Namespace == "Newtonsoft.Json.Serialization"
-                select type).Any();
-            if (newtonsoftNamespaceFound)
-                yield return "NEWTONSOFT_EXISTS";
-            
-            var naughtyAttributesNamespaceFound = (from type in types
-                where type.Namespace == "NaughtyAttributes"
-                select type).Any();
-            if (naughtyAttributesNamespaceFound)
-                yield return "NAUGHTYATTRIBUTES_EXISTS";
-            
-            var naughtyAttributesColorKeysNamespaceFound = (from type in types
-                where type.Namespace == "NaughtyAttributes.ColorKeyProperties"
-                select type).Any();
-            if (naughtyAttributesColorKeysNamespaceFound)
-                yield return "NAUGHTYATTRIBUTES_CK_EXISTS";
-            
-            var bindNamespaceFound = (from type in types
-                where type.Namespace == "Bind"
-                select type).Any();
-            if (bindNamespaceFound)
-                yield return "BIND_EXISTS";
-            
-            var addressablesNamespaceFound = (from type in types
-                where type.Namespace == "UnityEngine.ResourceManagement.AsyncOperations"
-                select type).Any();
-            if (addressablesNamespaceFound)
-                yield return "ADDRESSABLES_EXISTS";
-            
-            var vInspectorFound = (from type in types
-                    where type.Namespace == "VInspector"
-                    select type).Any();
-            if (vInspectorFound)
-                yield return "VINSPECTOR_EXISTS";
-            
-            var proceduralImageFound = (from type in types
-                where type.Namespace == "UnityEngine.UI.ProceduralImage"
-                select type).Any();
-            if (proceduralImageFound)
-                yield return "PROCEDURAL_IMAGE_EXISTS";
-            
-            var proceduralUIFound = (from type in types
-                where type.Namespace == "DTT.UI.ProceduralUI"
-                select type).Any();
-            if (proceduralUIFound)
-                yield return "PROCEDURAL_UI_EXISTS";
-            
-            var mmFeelFound = (from type in types
-                where type.Namespace == "MoreMountains.Feedbacks"
-                select type).Any();
-            if (mmFeelFound)
-                yield return "MMFEEL_EXISTS";
+            var names = assemblies.Select(assembly => assembly.GetName().Name).ToArray();
+            Array.Sort(names, StringComparer.Ordinal);
+            return string.Join(";", names);
+        }
+
+        private static IEnumerable<string> GetSymbols(Assembly[] assemblies)
+        {
+            var namespaces = new HashSet<string>(StringComparer.Ordinal);
+            foreach (var assembly in assemblies)
+            {
+                foreach (var type in GetTypes(assembly))
+                {
+                    if (type.Namespace != null)
+                        namespaces.Add(type.Namespace);
+                }
+            }
+
+            return NamespaceDefines
+                .Where(pair => namespaces.Contains(pair.Namespace))
+                .Select(pair => pair.Define);
+        }
+
+        private static IEnumerable<Type> GetTypes(Assembly assembly)
+        {
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException exception)
+            {
+                return exception.Types.Where(type => type != null);
+            }
         }
     }
 }
